f8720f8ccbbb575343f5dd8e5805056a7f75a0ea
Michal Sienkiewicz <s73271@beuth-hochschule.de>
2019-09-18 16:38:17 +0200

    exception handling

--- squad/views.py
+++ squad/views.py
@@ -36,33 +36,66 @@ class SquadBuilderView(View):
         '451': ['GK', 'LB', 'CB', 'CB', 'RB', 'LM', 'CDM', 'CAM', 'CDM', 'RM', 'ST']
     }
 
+    # get team value in milions
+    def get_team_value(self, team):
+        value = 0
+        for player in team:
+            value += player.value
+        return value/1000000
+
+    def get_team_overall(self, team):
+        overall = 0
+        for player in team:
+            overall += player.overall
+        return int(overall/len(team))
+
+    # Disable duplicates in case of repeting positions
+    def get_first_new_player(self, players, team):
+        if not players[0] in team:
+            return players[0]
+        elif not players[1] in team:
+            return players[1]
+        elif not players[2] in team:
+            return players[2]
+
+        # for player in players:
+        #     if not player in team:
+        #         return player
+
 
     def get(self, request):
-        budget = budget_left = float(request.GET.get('b')) * 1000000
-        team = []
+        try:
+            budget = budget_left = float(request.GET.get('b')) * 1000000
+            team = []
 
-        formation = request.GET.get('p')
-        positions = self.formations[formation]
+            formation = request.GET.get('p')
+            positions = self.formations[formation]
 
-        for pos in positions:
-            players_left = 11-len(team)
-            players = Player.objects.filter(
-                Q(value__lte = budget_left/players_left),
-                Q(position__exact = pos),
-                ~Q(value__exact = 0) # Ignore free players
-            )
-            # Select first player from list orderd by overall
-            player = players[0]
-            # Disable duplicates in case of repeting positions
-            if player in team:
-                player = players[1]
+            for pos in positions:
+                players_left = 11-len(team)
+                players = Player.objects.filter(
+                    Q(value__lte = budget_left/players_left),
+                    Q(position__exact = pos),
+                    ~Q(value__exact = 0) # Ignore free players
+                )
 
-            budget_left -= float(player.value)
-            team.append(player)
+                # Select first player from list orderd by overall
+                player = self.get_first_new_player(players, team)
 
-        # format budget, add thousend seperator (e.g: 1000000 to 1.000.000)
-        budget_str = "{:,}".format(budget).replace(',', '.')
-        # format float without trailing zeros
+                budget_left -= float(player.value)
+                team.append(player)
+
+
+        # if no players found (line 58) break the squad-builder loop
+        except IndexError:
+            team = None
+
+        # if wrong formation in query set defaulf foramtion
+        except KeyError:
+            formation = self.formations['433']
+
+
         budget = '{0:g}'.format(budget / 1000000)
 
-        return render(request, 'team.html', {'budget': budget, 'budget_left': budget_left, 'team': team})
+
+        return render(request, 'team.html', {'budget': budget, 'team': team, 'team_value': self.get_team_value(team), 'team_overall': self.get_team_overall(team)})
