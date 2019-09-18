c54e9f4ebf2581b4594620bd1f22771efc0268a7
Michal Sienkiewicz <s73271@beuth-hochschule.de>
2019-09-18 16:37:24 +0200

    url: squad builder in ''

--- squad/urls.py
+++ squad/urls.py
@@ -6,6 +6,5 @@ from django.views.generic.base import TemplateView
 urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('search/', search, name="search_results"),
-    path('squadbuilder/', TemplateView.as_view(template_name='squad_builder.html'), name='squadbuilder'),
-    path('squadbuilder/build/', SquadBuilderView.as_view(), name='build'),
+    path('build/', SquadBuilderView.as_view(), name='build'),
 ]
